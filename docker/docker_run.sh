2ac1eb32c6b77bef5a40d5e27fed8edb6841773b
cduarte <cduarte20109@gmail.com>
2024-08-18 02:30:22 -0400

    added more docker material

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,7 +1,20 @@
+# rocker --nvidia --x11 \
+# --env="ACCEPT_EULA=Y" \
+# --env="OMNI_SERVER=omniverse://127.0.0.1/NVIDIA/Assets/Isaac/4.1" \
+# --network=host \
+# --volume="./usr_persist_data/root/usr_data:/root/usr_data:rw" \
+# --volume="./isaac-sim-data/cache/kit:/isaac-sim/kit/cache/Kit:rw" \
+# --volume="./isaac-sim-data/cache/ov:/root/.cache/ov:rw" \
+# --volume="./isaac-sim-data/cache/pip:/root/.cache/pip:rw" \
+# --volume="./isaac-sim-data/cache/glcache:/root/.cache/nvidia/GLCache:rw" \
+# --volume="./isaac-sim-data/cache/computecache:/root/.nv/ComputeCache:rw" \
+# --volume="./isaac-sim-data/logs:/root/.nvidia-omniverse/logs:rw" \
+# --volume="./isaac-sim-data/data:/root/.local/share/ov/data:rw" \
+# --volume="./isaac-sim-data/documents:/root/Documents:rw" \
+# docker_nvidia_isaac_sim:latest
+
 rocker --nvidia --x11 \
 --env="ACCEPT_EULA=Y" \
---env="OMNI_SERVER=omniverse://127.0.0.1/NVIDIA/Assets/Isaac/4.1" \
---network=host \
 --volume="./usr_persist_data/root/usr_data:/root/usr_data:rw" \
 --volume="./isaac-sim-data/cache/kit:/isaac-sim/kit/cache/Kit:rw" \
 --volume="./isaac-sim-data/cache/ov:/root/.cache/ov:rw" \
