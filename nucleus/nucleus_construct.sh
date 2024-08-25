9f058f4ddd640a71599deee0d6fc816953c66aca
cduarte <cduarte20109@gmail.com>
2024-08-25 00:08:17 -0400

    mv on nucleus

--- nucleus/nucleus_construct.sh
+++
@@ -1,3 +0,0 @@
-LAUNCHER_PATH="ws/nucleus-workstation-2023.2.3.zip"
-SPLIT_PREFIX="ws/split"
-cat "${SPLIT_PREFIX}"* > "$LAUNCHER_PATH"
--- nucleus/nucleus_split.sh
+++
@@ -1,4 +0,0 @@
-LAUNCHER_PATH="ws/nucleus-workstation-2023.2.3.zip"
-SPLIT_PREFIX="ws/split"
-split --bytes=99M $LAUNCHER_PATH $SPLIT_PREFIX
-rm $LAUNCHER_PATH
